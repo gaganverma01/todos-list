75a462f8cf82a08067844ccb31dc659906a51f8b
gaganverma01 <vermagagan116@gmail.com>
2022-01-20 16:38:14 +0530

    Initial commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            This is my Components
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus eveniet fugiat velit officia. Itaque ratione voluptatibus ab eaque cum blanditiis laboriosam obcaecati officiis quas totam? Sed corporis neque soluta, ipsa labore officiis commodi quo?</p>
+        </div>
+    )
+}
